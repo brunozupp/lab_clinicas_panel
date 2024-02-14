f8a4525dffb8474bf6cfd924cbf21f9f84daef93
Bruno Noveli <brunimzuppwolf@gmail.com>
2024-02-14 16:06:08 -0300

    Finalizado primeira parte do Layout de painel

--- lib/src/pages/panel/panel_page.dart
+++ lib/src/pages/panel/panel_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_getit/flutter_getit.dart';
 import 'package:lab_clinicas_core/lab_clinicas_core.dart';
 import 'package:lab_clinicas_panel/src/pages/panel/panel_controller.dart';
+import 'package:lab_clinicas_panel/src/pages/panel/widgets/panel_main_widget.dart';
 
 class PanelPage extends StatefulWidget {
   const PanelPage({super.key});
@@ -22,6 +23,48 @@ class _PanelPageState extends State<PanelPage> with MessageViewMixin {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+
+    final sizeOf = MediaQuery.sizeOf(context);
+
+    return Scaffold(
+      appBar: LabClinicasAppBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 32,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: sizeOf.width * 0.4,
+                  child: const PanelMainWidget(
+                    passwordLabel: "Senha anterior",
+                    password: "BG5898",
+                    deskNumber: "03",
+                    labelColor: LabClinicasTheme.blueColor,
+                    buttonColor: LabClinicasTheme.orangeColor,
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                SizedBox(
+                  width: sizeOf.width * 0.4,
+                  child: const PanelMainWidget(
+                    passwordLabel: "Chamando senha",
+                    password: "BG5898",
+                    deskNumber: "03",
+                    labelColor: LabClinicasTheme.orangeColor,
+                    buttonColor: LabClinicasTheme.blueColor,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
